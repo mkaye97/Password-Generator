755911b5cb3a6b51a3220843d495cbbc71a093f3
mkaye <mattkaye97@gmail.com>
2022-11-27 21:10:17 -0500

    Addtiion of character sets to be used for password generation.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lettersLow = "abcdefghijklmnopqrstuvwxyz";
+var lettersCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"';
+
+lettersLow = lettersLow.split('');
+lettersCap = lettersCap.split('');
+specChars = specChars.split('');
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
